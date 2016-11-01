2b3c5753e67e0913daf9fba290f747ece0a68e8f
simon <jiahao@lightchaseranimation.com>
2016-11-01 12:41:44 -0400

    update company_information

--- config.py
+++ config.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+class Filter_Info():
+    def __init__(self):
+        self.education_set = {u"高中/中专/中技":0,
+                              u"大专":1,
+                              u"本科/学士":2,
+                              u"硕士/研究生":3,
+                              u"博士":4,
+                              u"其他":5,}
+
+        self.experience_set = {u"其他":0,
+                               u"无经验":1,
+                               u"1年以下":2,
+                               u"1-3年":3,
+                               u"3-5年":4,
+                               u"5-10年":5,
+                               u"10年以上":6,}
+
+        self.company_size_set = {u"20人以下":0,
+                                 u"20-99人":1,
+                                 u"100-499人":2,
+                                 u"500-999人":3,
+                                 u"1000-9999人":4,
+                                 u"10000人以上":5,
+                                 u"不限":6,}
+
+        self.company_type_set = {u"国企":0,
+                                 u"外商独资":1,
+                                 u"代表处":2,
+                                 u"合资":3,
+                                 u"民营":4,
+                                 u"股份制企业":5,
+                                 u"上市公司":6,
+                                 u"国家机关":7,
+                                 u"事业单位":8,
+                                 u"其它":9,}
+
 class Company(object):
 
     """Docstring for Company. """
@@ -8,10 +44,9 @@ class Company(object):
     def __init__(self):
         self.company_name = ""
         self.company_url = ""
-        self.company_type = ""
-        self.size_min = 0
-        self.size_max = 0
-        self.industry = ""
+        self.company_type = None
+        self.size = None
+        self.industry = None
         self.address = ""
         self.logitude = 0.0
         self.latitude = 0.0
--- page_logic.py
+++ page_logic.py
@@ -41,10 +41,146 @@ class Company_Info(Basic_Parse):
     def __init__(self,url):
         Basic_Parse.__init__(self,url)
         self.company_info = config.Company()
+        self.get_information()
+
+    def analyse_table(self,table):
+        table_set = {}
+        print len(table)
+        if len(table)%2:
+            print 'The comapny table is invaliable.'
+            return None
+        else:
+            #init table set
+            table_set = {}
+            for i in range(len(table)/2):
+                table_set[unicode(table[i*2].string.replace('：','').strip())] = unicode(table[i*2+1].string.strip())
+
+            #convert table_set
+            convert_set = {}
+            try:
+                #convert_set['company_type'] = table_set[u"公司性质"]
+                company_type_set = config.Filter_Info.comapny_type_set
+                company_type = table_set[u"公司性质"]
+                company_type_code = "%02d"%company_type_set[company_type]
+                convert_set['company_type'] = company_type_code
+            except:
+                convert_set['company_type'] = None
+
+            try:
+                #convert_set['company_size'] = table_set[u"公司规模"]
+                company_size_set = config.Filter_Info.company_size_set
+                company_size = table_set[u"公司规模"]
+                company_size_code = "%02d"%company_size_set[company_size]
+                convert_set['company_size'] = company_size_code
+            except:
+                convert_set['company_size'] = None
+
+            try:
+                convert_set['company_website'] = table_set[u"公司网站"]
+                if convert_set['company_website'] == '':
+                    convert_set['company_website'] = None
+            except:
+                convert_set['company_website'] = None
+
+            try:
+                convert_set['company_industry'] = []
+                company_industry_list = table_set[u"公司行业"].split(",")
+                industry_field_set = utilities.json_parse(r'industry_list.json')
+                for company_industry in company_industry_list:
+                    for i in range(len(industry_field_set)):
+                        fields = industry_field_set[i]["fields"]
+                        for j in range(len('fields')):
+                            if unicode(company_industry) == fields[j]:
+                                industry_code = "%03d%03d"%(i,j)
+                                convert_set['company_industry'].append(industry_code)
+                if convert_set['company_industry'] == []:
+                    convert_set['company_industry']= None
+            except:
+                convert_set['company_industry'] = None
+
+            try:
+                convert_set['company_address'] = table_set[u"公司地址"]
+                if convert_set['company_address'] == '':
+                    convert_set['company_address'] = None
+            except:
+                convert_set['company_address'] = None
+
+            return convert_set
+
+    def get_latlng(self):
+        map_content = self.bs_parse.find('table', 'comtinyDes').find_all('td')[-1].button['onclick']
+        if map_content:
+            map_content = map_content.replace('fnOpenMiniMap(','').replace(');','')
+            print map_content
+            info_list = map_content.split(', ')
+            lat = info_list[-2].replace('\'','')
+            lng = info_list[-1].replace('\'','')
+            return (lat,lng)
+        else:
+            return utilities.lat_lng(self.company_info.address)
+
+    def get_intro(self):
+        intro_content = ""
+        try:
+            intro = self.bs_parse.find('div', 'comapny-content').find_all(string = True)
+            for i in intro:
+                intro_content += i
+            if len(intro_content) >= 300:
+                return unicode(intro_content[:300])
+            else:
+                return unicode(intro_content)
+        except:
+            return None
+
+
+    def show_info(self):
+        print 'name: {}'.format(self.company_info.company_name)
+        print 'url: {}'.format(self.company_info.company_url)
+        print 'type: {}'.format(self.company_info.company_type)
+        print 'size: {}'.format(self.company_info.size)
+        print 'industry&field: {}'.format(self.company_info.industry)
+        print 'address: {}'.format(self.company_info.address)
+        print 'lat&lng: {lat},{lng}'.format(lat = self.company_info.latitude, lng = self.company_info.logitude)
+        print 'website: {}'.format(self.company_info.website)
+        print 'info: '
+        print self.company_info.info
+
+
+
+    def get_information(self):
+        main_content = self.bs_parse.find('div', 'mainLeft')
+        self.company_info.company_name = unicode(main_content.find('h1').string).strip()
+
+        print 'process %s company.'%self.company_info.company_name
+        self.company_info.company_url = self.url
+
+        company_table = main_content.find('table', 'comTinyDes').find_all('span')
+        company_table = self.analyse_table(company_table)
+        print company_table
+
+        self.company_info.company_type = company_table['company_type']
+        self.company_info.size = company_table['company_size']
+        self.company_info.industry = company_table['company_industry']
+        self.company_info.address = company_table['company_address']
+        self.company_info.website = company_table['company_website']
+
+        #get lat&lng
+        self.company_info.latitude, self.company_info.logitude = self.get_latlng()
+
+        #get company introduction
+        self.company_info.info= self.get_intro()
 
 
 
 if __name__=="__main__":
-    companies_url= Company_List(r'http://company.zhaopin.com/changsha/')
-    print companies_url.page_list
-    print companies_url.next_page_url
+#    companies_url= Company_List(r'http://company.zhaopin.com/changsha/')
+#    print companies_url.page_list
+#    print companies_url.next_page_url
+    company_info = Company_Info(r'http://company.zhaopin.com/CC466748528.htm')
+    company_info.show_info()
+
+
+
+
+
+
